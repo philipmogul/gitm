08d00b933c6c378eaf7d6c25605af0c14fdd754b
philipvalue <philipp1mogul@gmail.com>
2025-06-24 13:20:40 +0300

    Using Clones and Pulls in Git

---
+++ cloning.php
@@ -0,0 +1,15 @@
+To clone a repo, cd into folder to import that repo into and, use: 
+
+// C:cd foldername 
+
+git clone urlname 
+
+eg 
+
+git clone https://github.com/philipmogul/gitm.git
+
+
+It will import full project into the folder 
+
+
+
